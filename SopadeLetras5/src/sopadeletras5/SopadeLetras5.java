3f4aabb9204cb347c8d96deb57a0c2a280895dd3
Kevin Zhang <kazp058@gmail.com>
2021-11-27 00:08:52 -0500

    Implementacion de Controlador de ventanas

--- SopadeLetras5/src/sopadeletras5/SopadeLetras5.java
+++ SopadeLetras5/src/sopadeletras5/SopadeLetras5.java
@@ -21,34 +21,35 @@ import javafx.stage.Stage;
  * @author milca
  */
 public class SopadeLetras5 extends Application {
-    
+
     List<List<Character>> matrix;
-    
+    ScreenController screenController;
+
     @Override
     public void start(Stage primaryStage) {
-                
-        Tablero tablero = new Tablero(5,4);
-        
-        tablero.getMatrix().put(1,2,'Z');
-        System.out.println(tablero.getMatrix());
-        
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(tablero.getGrid());
-        
-        Scene scene = new Scene(root, 900, 600);
-        
+
+        Tablero tablero = new Tablero(5, 4);
+
+//        tablero.getMatrix().put(1,2,'Z');s
+//        System.out.println(tablero.getMatrix());
+//        
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(tablero.getGrid());
+//        
+        screenController = new ScreenController(900,600);
+
         primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(screenController.getContext());
         primaryStage.show();
     }
 
@@ -58,5 +59,5 @@ public class SopadeLetras5 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
